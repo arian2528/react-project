b47495c2387af52e2b4b9dc7837d51cb954c125f
arian2528 <arian2528@gmail.com>
2017-07-04 12:02:04 -0400

    Bulding header and Routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Faq from './components/Faq/Faq';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +12,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-
+        	<Route exact={true} path="/" component={Home} />
         <Footer />
       </div>
     );
