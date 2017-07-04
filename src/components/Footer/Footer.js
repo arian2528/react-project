ba344c73be392730429cd4bc018ce13e965beac7
arian2528 <arian2528@gmail.com>
2017-07-04 12:19:20 -0400

    Footer html and footer css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
   
     return (
-      <div className="App">
-      	My Footer
+      <div>
+      	<footer className="footer is-primary">
+	      <div className="container">
+	        <div className="columns">
+	          <div className="column">
+	            <p>My footer goes here</p>
+	          </div>
+	          <div className="column has-text-right">
+	            <a className="icon" href="#"><i className="fa fa-facebook"></i></a>
+	            <a className="icon" href="#"><i className="fa fa-twitter"></i></a>
+	          </div>
+	        </div>
+	      </div>
+	    </footer>
       </div>
     );
   
