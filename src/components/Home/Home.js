9e46947560d7f5dd0b4651e5d5996a31349e2e92
arian2528 <arian2528@gmail.com>
2017-07-04 12:58:58 -0400

    Fixing footer css import. Adding hero section and comments on home page. Adding home page css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,68 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 class Home extends Component {
+	
+
 	render() {
+
+		let heading = "Soaring to new heights";
+		let subheading = "My subheading";
+
 		return (
 			<div>
-				Home
+				<section className="hero">
+			      <div className="hero-body">
+			      <div className="container">
+			        <h1 className="title">{ heading }</h1>
+			        <div className="is-two-thirds column is-paddingless">
+			          <h2 className="subtitle is-4">{ subheading }</h2>
+			        </div>
+			        <a className="button is-large is-primary" id="learn">Learn more</a>
+			      </div>
+			      </div>
+			    </section>
+			    <section className="section">
+			      <div className="container">
+			        <div className="columns pd is-desktop">
+			          <div className="column is-1 has-text-centered">
+			            <i className="fa fa-cog is-primary"></i>
+			          </div>
+			          <div className="column is-one-third-desktop has-text-centered">
+			            <p className="title"><strong>We provided superior business</strong></p>
+			          </div>
+			          <div className="column">
+			            <p>Lorem ipsum</p>
+			          </div>
+			        </div>
+			        <div className="column pd">
+			          <div className="column">
+			            <div className="card">
+			              <div className="card-content">
+			                <p className="title">The best of the best</p>
+			                <p className="subtitle">Arian Buzon</p>
+			              </div>
+			            </div>
+			          </div>
+			          <div className="column">
+			            <div className="card">
+			              <div className="card-content">
+			                <p className="title">The best of the best</p>
+			                <p className="subtitle">Arian Buzon</p>
+			              </div>
+			            </div>
+			          </div>
+			          <div className="column">
+			            <div className="card">
+			              <div className="card-content">
+			                <p className="title">The best of the best</p>
+			                <p className="subtitle">Arian Buzon</p>
+			              </div>
+			            </div>
+			          </div>
+			        </div>
+			      </div>
+			    </section>
 			</div>
 		);
 	}
